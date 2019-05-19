ab5e2ba07a4f22bd385d0318d0f5b3553a3529a0
Phoder1 <Phoder1@users.noreply.github.com>
2019-05-20 00:12:39 +0300

    LEVEL 2!

---
+++ Assets/Scripts/Wheel_script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel_script : MonoBehaviour
+{
+    [SerializeField] float Turnspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * Turnspeed * Time.deltaTime);
+        
+    }
+}
