7064ab29d1465d8c15b4163df6ec234e061b5a80
Phoder1 <Phoder1@users.noreply.github.com>
2019-05-19 19:55:39 +0300

    Level 1

--- Assets/Scripts/Rocket_script.cs
+++ Assets/Scripts/Rocket_script.cs
@@ -5,23 +5,29 @@ using UnityEngine;
 
 public class Rocket_script : MonoBehaviour
 {
-    [SerializeField] bool debug;
+    [SerializeField] bool debugon = false;
     [SerializeField] float rotationfriction = 0.3f;
     [SerializeField] float destroyforce = 4f;
     public float thrustforce;
     [SerializeField] float thrustpressforce;
     [SerializeField] float rotationsens = 2f;
-    [SerializeField] AudioSource boostersound;
+    [SerializeField] AudioSource Boostersound;
     [SerializeField] AudioSource collisionsound;
     [SerializeField] AudioSource scratchsound;
     [SerializeField] AudioSource finishsound;
     private float rotationspeed = 0f;
+    private float slowforce = 0f;
+    [SerializeField] float maxslowspeed = 20f;
+    public static Rocket_script instance;
     Rigidbody rb;
     Collider col;
-
+    private float vely = 0f;
+    private float velx = 0f;
+    private Vector3 transformspeed;
     // Start is called before the first frame update
     void Start()
     {
+        instance = this;
         rb = GetComponent<Rigidbody>();
         col = GetComponent<Collider>();
         destroyforce *= rb.mass;
@@ -30,11 +36,11 @@ public class Rocket_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
         Handlecontrols();
-        if (debug) { Debugon(); }
-        
-        
+        if (debugon) { Debugon(); }
+
+
     }
 
     private void Debugon()
@@ -44,7 +50,7 @@ public class Rocket_script : MonoBehaviour
         Debug.Log("Rotation Sens :" + rotationsens);
     }
 
-    void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.relativeVelocity.magnitude >= destroyforce)
         {
@@ -66,43 +72,57 @@ public class Rocket_script : MonoBehaviour
             }
         }
     }
-    void Handlecontrols()
-
-    
+    private void Handlecontrols()
     {
 
         rotationspeed -= Input.GetAxis("Horizontal") * rotationsens * Time.deltaTime;
         transform.Rotate(transform.forward, rotationspeed);
-        rotationspeed *= Mathf.Pow((1f-(rotationfriction/100)) , Time.deltaTime);
+        rotationspeed *= Mathf.Pow((1f - (rotationfriction / 100)), Time.deltaTime);
 
         if (Input.GetButton("Jump"))
         {
-            rb.AddForce(rb.mass * transform.up * thrustforce * Time.deltaTime * (Convert.ToInt32(Input.GetButtonDown("Jump")) * (thrustpressforce-1) + 1));
-            if (!boostersound.isPlaying)
+            rb.AddForce(rb.mass * transform.up * thrustforce * Time.deltaTime * (Convert.ToInt32(Input.GetButtonDown("Jump")) * (thrustpressforce - 1) + 1));
+            if (!Boostersound.isPlaying)
             {
-                boostersound.Play();
+                Boostersound.Play();
             }
-            if(boostersound.volume != 1)
+            if (Boostersound.volume != 1)
             {
-                boostersound.volume = 1;
+                Boostersound.volume = 1;
             }
         }
         else
         {
-            if (boostersound.volume <= 0.01)
+            if (Boostersound.volume <= 0.01)
             {
-                boostersound.Stop();
-            }else
+                Boostersound.Stop();
+            }
+            else
             {
-                boostersound.volume -= 1 * Time.deltaTime;
+                Boostersound.volume -= 1 * Time.deltaTime;
             }
 
 
         }
 
     }
-    
-    
+
+    public void Slowmode(float amount)
+    {
+        if (amount == 0f)
+        {
+            slowforce = 0f;
+        }
+        else
+        {
+            slowforce = (1 - Mathf.Max(((amount - 300f) / 60f), 0f));
+        }
+        transformspeed = transform.TransformVector(Vector3.up * Vector3.Dot(rb.velocity, transform.up.normalized));
+        //rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y * Mathf.Pow((1 - (slowforce * (1 - Mathf.Abs(transformspeed.normalized.y)) * maxslowspeed / 100)), Time.deltaTime), 0f ) * Mathf.Pow((1 - (slowforce * (1 - Mathf.Abs(transformspeed.normalized.y)) * maxslowspeed / 100)), Time.deltaTime);
+        vely = rb.velocity.y * Mathf.Pow(1f - (maxslowspeed * slowforce * Mathf.Abs(transformspeed.normalized.y)) / 100, Time.deltaTime);
+        velx = rb.velocity.x * Mathf.Pow(1f - (maxslowspeed * slowforce * Mathf.Abs(transformspeed.normalized.x)) / 100, Time.deltaTime);
+        rb.velocity = new Vector3(velx, vely, 0f);
+    }
 
 }
 
---
+++ Assets/Scripts/Wings_script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wings_script : MonoBehaviour
+{
+    [SerializeField] float opentime = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Rocket_script.instance.Slowmode(transform.localEulerAngles.z);
+            if (transform.localEulerAngles.z > 300f || transform.localEulerAngles.z == 0f)
+            {
+
+                transform.Rotate(-Vector3.forward * 45f * (Time.deltaTime / opentime), Space.Self);
+            }else
+            {
+                transform.localEulerAngles = Vector3.forward * 300f + Vector3.up * transform.localEulerAngles.y;
+            }
+        }
+        else if (transform.localEulerAngles.z < 360f && transform.localEulerAngles.z > 250f)
+        {
+            transform.Rotate(Vector3.forward * 45f * (Time.deltaTime / opentime), Space.Self);
+        }
+        else
+        {
+            transform.localEulerAngles = Vector3.forward * 0f + Vector3.up * transform.localEulerAngles.y;
+        }
+
+    }
+}
